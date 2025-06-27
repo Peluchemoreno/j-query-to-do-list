99a4c18b9219846cc60cc837cb8b6ecb70c801b1
justin <justin@sharktub.v2>
2025-06-27 09:35:40 -0500

    refactor and clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,29 @@
 function newItem() {
-
-
-  let li = $('<li></li>');
-  let inputValue = $('#input').val();
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
   li.append(inputValue);
 
-  if (inputValue === '') {
+  if (inputValue === "") {
     alert("You must write something!");
   } else {
-    $('#list').append(li);
-  };
+    $("#list").append(li);
+  }
 
   function strikeOut() {
-    li.toggleClass('strike');
-  };
+    li.toggleClass("strike");
+  }
 
-  li.on('dblclick', function strikeOut() {
-    li.toggleClass('strike');
-  });
+  function removeItem() {
+    li.addClass("delete");
+  }
+
+  li.on("dblclick", strikeOut);
 
   let removeBtn = $('<button class="crossOutButton"></button>');
-  removeBtn.append(document.createTextNode('X'));
+  removeBtn.append(document.createTextNode("X"));
   li.append(removeBtn);
 
-  removeBtn.on('click', function removeItem() {
-    li.addClass('delete');
-  });
+  removeBtn.on("click", removeItem);
 
-  $('#list').sortable();
+  $("#list").sortable();
 }
